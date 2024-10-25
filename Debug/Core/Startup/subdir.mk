d62a7136c4a55a0b96d95c2dadd22fa515ad8365
alext2052 <alextong2004@gmail.com>
2024-10-25 11:55:58 -0500

    External oscillator

--- Debug/Core/Startup/subdir.mk
+++ Debug/Core/Startup/subdir.mk
@@ -1,6 +1,6 @@
 ################################################################################
 # Automatically-generated file. Do not edit!
-# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
@@ -16,7 +16,7 @@ S_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
-	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -I"C:/ST/STM32CubeIDE_1.10.1/vim_rev0/Core" -I"C:/ST/STM32CubeIDE_1.10.1/gophercan-lib" -I"C:/ST/STM32CubeIDE_1.10.1/gophercan-lib/network_autogen" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -I"C:/ST/STM32CubeIDE_1.10.1/vim_fsae_rev0/Core" -I"C:/ST/STM32CubeIDE_1.10.1/gophercan-lib" -I"C:/ST/STM32CubeIDE_1.10.1/gophercan-lib/network_autogen" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
 
 clean: clean-Core-2f-Startup
 
